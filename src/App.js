08fb08cfedd4350f6bf755b8aeebcda0a944e0d5
Yash-5865 <yashamin14@gmail.com>
2024-05-06 19:42:15 -0400

    test 2

--- src/App.js
+++ src/App.js
@@ -15,35 +15,100 @@ import NFT from './abis/NFT.json'
 import config from './config.json';
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
+  const [provider, addProvider] = useState(null)
+  const [account, addAccount] = useState(null)
 
-  const loadBlockchainData = async () => {
+  const [image,addImage] = useState(null)
+  const [name,addName] = useState("")
+  const [description,addDescription] = useState("")
+  const[url,addReturn] = useState(null)
+  
+  const Blockchain = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
+    addProvider(provider)
   }
 
+  const submitForm = async (e) => {
+    e.preventDefault()
+    const AI_Image = newImage()
+    const To_Contract = await ImageToContract(AI_Image)
+    console.log(To_Contract)
+  }
+
+  const newImage = async () => {
+    try {
+      console.log("New Image");
+  
+      const response = await axios.post(
+        "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2",
+        {
+          inputs: description,
+          options: { wait_for_model: true },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          responseType: "arraybuffer",
+        }
+      );
+  
+      const dt = response.data;
+      const returnImage = response.headers["content-type"];
+  
+      const new_dt = Buffer.from(dt).toString("base64");
+      const obj = `data:${returnImage};base64,${new_dt}`;
+  
+      addImage(obj);
+  
+      return dt;
+    } catch (error) {
+      console.error("Error fetching image:", error);
+      // Handle the error appropriately (e.g., show an error message to the user)
+    }
+  };
+
+  const ImageToContract = async (AI_Image) => {
+    console.log("Adding image to contract")
+    const store1 = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE_API_KEY })
+    
+    const { nft_ip } = await store1.store({
+      image: new File([AI_Image], "image.jpeg", { type: "image/jpeg" }),
+      name: name,
+      description: description,
+    })
+
+    const returnValue = `https://ipfs.io/ipfs/${nft_ip}/metadata.json`
+
+    addReturn(returnValue)
+
+    return returnValue
+
+  };
+
   useEffect(() => {
-    loadBlockchainData()
+    Blockchain()
   }, [])
 
   return (
     <div>
-      <Navigation account={account} setAccount={setAccount} />
+      <Navigation account={account} setAccount={addAccount} />
       
       <div className='form'>
-        <form>
-          <input type="text" placeholder='Create a Name' />
-          <input type="text" placeholder='Create a Description'/>
+        <form onSubmit={submitForm}>
+          <input type="text" placeholder='Create a Name' onChange={(e) => {addName(e.target.value)}}/>
+          <input type="text" placeholder='Create a Description' onChange={(e) =>{addDescription(e.target.value)}}/>
           <input type="submit" value="Mint AI NFT"/>
         </form>
         
         <div className='image'>
-          <image src="" alt="AI NFT" />
+          <img src={image} alt="AI NFT" />
         </div>
       </div>
 
-      <p>View&nbsp;<a href="" target="_blank" rel="noreferrer">Metadata</a></p>
+      <p>View&nbsp;<a href={url} target="_blank" rel="noreferrer">Metadata</a></p>
 
     </div>
   );
